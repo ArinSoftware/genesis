f9be0a8c2c12af5624ffa364dcd52accb21e1428
Arin <arinyazilim@gmail.com>
2020-04-06 23:04:30 +0300

    Ders 10

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+/* SCROLL REVEAL ANIMATIONS */
+
+ScrollReveal().reveal('.ani-left', {
+    origin: 'left',
+    distance: '10rem',
+    duration: 1000,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('.ani-right', {
+    origin: 'right',
+    distance: '10rem',
+    duration: 1000,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('.ani-fade-up', {
+    opacity: 0,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('.ani-bottom', {
+    origin: 'bottom',
+    distance: '10rem',
+    duration: 1000,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('.ani-delay', {
+    delay: 500
+});
+
+ScrollReveal().reveal('.ani-delay-2', {
+    delay: 1000
+});
+
+
+
+
