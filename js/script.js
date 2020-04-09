8fc943d5da6fa642a4196c615400ebcb9dafbd97
Arin <arinyazilim@gmail.com>
2020-04-09 13:32:12 +0300

    Ders 12

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+/* RESPONSIVE NAV MENU  */
+
+const navMenu = () => {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navList = document.querySelector('.nav-list');
+
+    navbarToggler.addEventListener('click', () => {
+        navList.classList.toggle('nav-active');
+        navbarToggler.classList.toggle('toggle');
+    })
+};
+
+navMenu();
+
+/* SERVICES SLIDER */
+
+    const serviceChange = () => {
+        let firstChild, lastChild;
+        const arrowLeft = document.querySelector('#arrow-left');
+        const arrowRight = document.querySelector('#arrow-right');
+        const servicesAll = document.querySelector('#services-slider')
+
+        document.addEventListener('click', () => {
+            if (event.target === arrowLeft) {
+                lastChild = servicesAll.lastElementChild;
+                servicesAll.insertAdjacentElement('afterbegin', lastChild)
+            } else if (event.target === arrowRight) {
+                firstChild = servicesAll.firstElementChild;
+                servicesAll.insertAdjacentElement('beforeend', firstChild)
+            }
+        })
+    }
+
+serviceChange();
+
+
+
+
 
 /* SCROLL REVEAL ANIMATIONS */
 
